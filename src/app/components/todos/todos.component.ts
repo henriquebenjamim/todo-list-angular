ba5c7aa17c43047dcb945f8e9999a2aaf0b3689c
henriquebenjamim <henriiqqq@gmail.com>
2022-08-17 18:11:51 -0300

    Project complete

--- src/app/components/todos/todos.component.ts
+++ src/app/components/todos/todos.component.ts
@@ -10,6 +10,8 @@ export class TodosComponent implements OnInit {
 
   todos:Todo[];
 
+  inputTodo:string = "";
+
   constructor() { }
 
   ngOnInit(): void {
@@ -35,5 +37,14 @@ export class TodosComponent implements OnInit {
 
   deleteTodo (id: number) {
     this.todos = this.todos.filter((v, i) => i !== id);
+  }
+
+  addTodo () {
+    this.todos.push({
+      content: this.inputTodo,
+      completed: false
+    });
+
+    this.inputTodo = "";
   }
 }
